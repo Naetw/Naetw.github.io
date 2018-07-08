54a45cf0469ecbac0d5448827165248a72ad2b32
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2018-07-08 11:39:00 +0800

    Add some basic settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,13 +3,11 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Naetw'
-SITENAME = 'Naetw'
-SITEURL = 'naetw.github.io'
-
+SITENAME = 'naetw\'s blog'
+SITEURL = 'https://naetw.github.io'
 PATH = 'content'
-
 TIMEZONE = 'Asia/Taipei'
-
+THEME = 'theme/plumage'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -20,16 +18,32 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+GITHUB_USERNAME = 'Naetw'
+SOCIAL = (('GitHub', 'https://github.com/{}'.format(GITHUB_USERNAME)),)
 
 DEFAULT_PAGINATION = 10
+
+# URL
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = TAG_URL + 'index.html'
+
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'
+
+TAGS_SAVE_AS = 'tags/index.html'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+
+DISPLAY_CATEGORIES_ON_MENU = False
+MENUITEMS = [('Archives', '/archives'),
+             ('Categories', '/categories'),
+             ('Tags', '/tags'),]
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'naetw.github.io'
+SITEURL = 'https://naetw.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
