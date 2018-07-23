06598289c428d978bac0965f5067d9eefa50dcbb
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2018-07-23 11:21:17 +0800

    Add Disqus

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 GOOGLE_ANALYTICS = "UA-122098105-1"
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = "naetw-github-io"
